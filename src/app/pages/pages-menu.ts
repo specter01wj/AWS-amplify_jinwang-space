12e96309ccd89c47b6da3e47eaf75bc8e04cbedd
Jin Wang <specter01wj@gmail.com>
2023-11-23 23:15:48 -0500

    v1

--- src/app/pages/pages-menu.ts
+++ src/app/pages/pages-menu.ts
@@ -17,7 +17,7 @@ export const MENU_ITEMS: NbMenuItem[] = [
     group: true,
   },
   {
-    title: 'My Demo Projects',
+    title: 'My Project Playground',
     icon: 'briefcase-outline',
     children: [
       {
